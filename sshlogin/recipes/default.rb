954e6df222ccca4c3d6bfa87a1731d845ece6065
Andy <andy@brkt.com>
2014-09-18 19:13:50 -0400

    Changes to sshlogin cookbook

--- sshlogin/recipes/default.rb
+++ sshlogin/recipes/default.rb
@@ -8,12 +8,12 @@
 #
 # Create empty RSA password
 execute "ssh-keygen" do
-  command "sudo -u hadoop ssh-keygen -q -t rsa -N '' -f /home/hadoop/.ssh/id_rsa"
-  creates "/home/hadoop/.ssh/id_rsa"
+  command "sudo -u ubuntu ssh-keygen -q -t rsa -N '' -f /home/ubuntu/.ssh/id_rsa"
+  creates "/home/ubuntu/.ssh/id_rsa"
   action :run
 end
 
 # Copy public key to node1; if key doesn't exist in authorized_keys, append it to this file
 execute <<EOF
-cat /home/hadoop/.ssh/id_rsa.pub | sudo -u hadoop ssh hadoop@node1 "(cat > /tmp/tmp.pubkey; mkdir -p .ssh; touch .ssh/authorized_keys; grep #{node[:fqdn]} .ssh/authorized_keys > /dev/null || cat /tmp/tmp.pubkey >> .ssh/authorized_keys; rm /tmp/tmp.pubkey)"
+cat /home/ubuntu/.ssh/id_rsa.pub | sudo -u ubuntu ssh ubuntu@node1 "(cat > /tmp/tmp.pubkey; mkdir -p .ssh; touch .ssh/authorized_keys; grep #{node[:fqdn]} .ssh/authorized_keys > /dev/null || cat /tmp/tmp.pubkey >> .ssh/authorized_keys; rm /tmp/tmp.pubkey)"
 EOF
